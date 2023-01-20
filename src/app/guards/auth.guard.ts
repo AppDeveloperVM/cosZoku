769cfdcae15cc7c5e6373be55a7f0eace3ea6a37
AppDeveloperVM <vicmr1994@gmail.com>
2023-01-20 22:25:58 +0100

    update authGuard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,21 +1,22 @@
 import { Injectable } from '@angular/core';
-import { CanLoad, Route, Router, UrlSegment, UrlTree } from '@angular/router';
+import { CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanLoad {
+export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) { }
 
-  canLoad(
+  canActivate(
     route: Route,
-    segments: UrlSegment[]
+    state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | boolean {
     if (!this.authService.isLoggedIn) {
       this.router.navigateByUrl('/login');
       console.log('Not logged In, redirect to Login');
+      return false;
     }
     return true;
   }
