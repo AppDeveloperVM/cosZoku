f356d46f1e798936e8f39d4b44ac068d8a1d8201
AppDeveloperVM <vicmr1994@gmail.com>
2023-01-20 22:26:30 +0100

    Update team.service.ts

--- src/app/services/team/team.service.ts
+++ src/app/services/team/team.service.ts
@@ -14,22 +14,20 @@ export class TeamService {
   teams$ : Observable<Team[]>;
   teamsCollection : AngularFirestoreCollection<any>;
   private usersCollection : AngularFirestoreCollection<any>;
-  private userId = this.Auth.currentUser.uid;
+  private userId = this.authService.userUid;
 
-  constructor(private readonly afs : AngularFirestore, private authService: AuthService, private Auth: Auth) {
-    
-    this.usersCollection = afs.collection<ProfileUser>('users');
-    
-    this.teamsCollection = this.usersCollection.doc(this.userId).collection<Team>('teams');
-    this.getTeams( this.userId );
+  constructor(
+    private readonly afs : AngularFirestore, private authService: AuthService, private Auth: Auth
+  ) {
+      this.usersCollection = afs.collection<ProfileUser>('users');    
+      this.teamsCollection = this.usersCollection.doc(this.userId).collection<Team>('teams');
+      this.getTeams( this.userId );
   }
 
   public getTeams(userId: String): void {
-    const collectionPath = "/users/"+ userId +"/teams";    
     this.teams$ = this.teamsCollection?.snapshotChanges().pipe(
       map( actions => actions.map( a => a.payload.doc.data() as Team))
     )
-
   }
 
   saveTeam(team: Team, teamId: string): Promise<void> {
