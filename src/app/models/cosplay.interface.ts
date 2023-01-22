191cf99966faadb66dcf6325b20f87c555c0f196
AppDeveloperVM <vicmr1994@gmail.com>
2023-01-22 14:45:19 +0100

    Update cosplay.interface.ts

--- src/app/models/cosplay.interface.ts
+++ src/app/models/cosplay.interface.ts
@@ -1,11 +1,11 @@
 export class Cosplay {
     constructor(
         public id: string,
-        public creationDate: Date,
         public characterName: string,
         public description: string,
-        public imageUrl: string,
         public series: string,
+        public imageUrl: string,
+        public creationDate: Date,
         public funds: number,
         public percentComplete: string,
         public status: boolean,
