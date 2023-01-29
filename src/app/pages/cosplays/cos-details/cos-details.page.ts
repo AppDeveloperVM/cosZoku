aa994ef0e3bc4636da1d13533edf93ede94926f1
AppDeveloperVM <vicmr1994@gmail.com>
2023-01-29 19:47:14 +0100

    update cos Details

--- src/app/pages/cosplays/cos-details/cos-details.page.ts
+++ src/app/pages/cosplays/cos-details/cos-details.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { Cosplay } from 'src/app/models/cosplay.interface';
+import { CosplayService } from 'src/app/services/cosplay/cosplay.service';
 
 @Component({
   selector: 'app-cos-details',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cos-details.page.scss'],
 })
 export class CosDetailsPage implements OnInit {
+  cosplay: Cosplay;
+  isLoading = false;
+  imageReady = false;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private cosService: CosplayService) { }
 
   ngOnInit() {
+    this.getCosplay();
+  }
+
+  getCosplay(): void {
+    this.isLoading = true;
+    const id = this.route.snapshot.paramMap.get('id');
+    this.cosService.getCosplay(id).then(
+      (cosplay) => {
+        if(cosplay){
+          this.cosplay = cosplay.data();
+        } else {
+          console.log('ERROR');
+        }
+        this.imageReady = true;
+        this.isLoading = false;
+      }
+    )
   }
 
 }
