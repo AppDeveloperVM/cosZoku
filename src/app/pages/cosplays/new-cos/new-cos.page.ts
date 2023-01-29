3f778178f961bb0073ffb5460ec51011a4ae037e
AppDeveloperVM <vicmr1994@gmail.com>
2023-01-29 19:47:07 +0100

    updated new Cos Page

--- src/app/pages/cosplays/new-cos/new-cos.page.ts
+++ src/app/pages/cosplays/new-cos/new-cos.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CosplayService } from 'src/app/services/cosplay/cosplay.service';
 
 @Component({
   selector: 'app-new-cos',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-cos.page.scss'],
 })
 export class NewCosPage implements OnInit {
+  newCosForm: FormGroup;
+  isFormReady = true;
 
-  constructor() { }
+  constructor(private cosService: CosplayService, private router: Router) { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  initForm(){
+    this.newCosForm = new FormGroup({
+      characterName: new FormControl('', { validators: [ Validators.required ]}),
+      series: new FormControl('', { validators: [ Validators.required ]}),
+      description: new FormControl('', { validators: [ Validators.required ]}),
+      imageUrl: new FormControl( null )
+    });
+  }
+
+  uploadInage(){
+    this.isFormReady = false;
+    // ...
+    this.isFormReady = true;
+  }
+
+  onSubmitCosplay(){
+    this.cosService.saveCosplay(this.newCosForm.value)
+    .then((res)=> {
+      this.router.navigate(['/home']);
+    })
+    
   }
 
 }
