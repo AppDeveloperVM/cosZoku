b08e899df81a20f3caf866df95b3e799ec2cc87c
AppDeveloperVM <vicmr1994@gmail.com>
2023-02-09 22:48:58 +0100

    Added Edit Cos - inside Cos details

--- src/app/pages/cosplays/cos-details/cos-details.page.ts
+++ src/app/pages/cosplays/cos-details/cos-details.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Route, Router } from '@angular/router';
 import { Cosplay } from 'src/app/models/cosplay.interface';
 import { CosplayService } from 'src/app/services/cosplay/cosplay.service';
@@ -11,18 +12,14 @@ import { CosplayService } from 'src/app/services/cosplay/cosplay.service';
 export class CosDetailsPage implements OnInit {
   cosplay: Cosplay;
   isLoading = false;
+  isEditEnabled = false;
   imageReady = false;
+  detailsForm: FormGroup;
 
   constructor(private route: ActivatedRoute, private cosService: CosplayService) { }
 
   ngOnInit() {
-    this.getCosplay();
-  }
-
-  getCosplay(): void {
-    this.isLoading = true;
-    const id = this.route.snapshot.paramMap.get('id');
-    this.cosService.getCosplay(id).then(
+    this.getCosplay().then(
       (cosplay) => {
         if(cosplay){
           this.cosplay = cosplay.data();
@@ -31,8 +28,35 @@ export class CosDetailsPage implements OnInit {
         }
         this.imageReady = true;
         this.isLoading = false;
+        this.initForm();
       }
     )
+  }
+
+  getCosplay() {
+    this.isLoading = true;
+    const id = this.route.snapshot.paramMap.get('id');
+    return this.cosService.getCosplay(id)
+  }
+
+  initForm() {
+    this.detailsForm = new FormGroup({
+      characterName: new FormControl(this.cosplay.characterName),
+      series: new FormControl(this.cosplay.series),
+      description: new FormControl(this.cosplay.description)
+    });
+  }
+
+  enableEdit() {
+    this.isEditEnabled = true;
+  }
+  saveChanges() {
+    this.isEditEnabled = false;
+
+    this.cosService.saveCosplay(this.detailsForm.value, this.cosplay.id)
+    .then((res)=> {
+      alert('Saved succesfully');
+    })
   }
 
 }
