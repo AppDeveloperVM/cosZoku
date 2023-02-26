5aa88b7db01cd578834703f94312ebc5fd501484
AppDeveloperVM <vicmr1994@gmail.com>
2023-02-26 14:09:27 +0100

    Update shared.module.ts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';  
 
 import { ImagePickerComponent } from './image-picker/image-picker.component';
 import { ImageCropperModule } from 'ngx-image-cropper';
@@ -7,7 +8,7 @@ import { ImageCropperModule } from 'ngx-image-cropper';
 
 @NgModule({
     declarations: [ImagePickerComponent],
-    imports: [IonicModule, ImageCropperModule ],
+    imports: [CommonModule, IonicModule, ImageCropperModule ],
     exports: [ImagePickerComponent],
 })
 
